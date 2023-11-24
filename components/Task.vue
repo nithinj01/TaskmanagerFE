2915c45fc227778097dff2fd85a5708774e015dd
Nithin J <47292784+nithinj01@users.noreply.github.com>
2023-11-24 22:36:10 +0100

    intital commit

---
+++ components/Task.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <p>{{ task.name }}</p>
+    <button @click="completeTask">Complete Task</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      task: {
+        id: 1,
+        name: 'Example Task',
+      },
+    };
+  },
+  methods: {
+    completeTask() {
+      // Ensure that this.onComplete is a function
+      if (typeof this.onComplete === 'function') {
+        // Call the onComplete method with the task as an argument
+        this.onComplete(this.task);
+      } else {
+        console.error('onComplete is not a function');
+      }
+    },
+  },
+};
+</script>
---
+++ components/TaskList.vue
@@ -0,0 +1,25 @@
+<!-- components/TaskList.vue -->
+<template>
+  <div>
+    <task v-for="task in tasks" :key="task.id" :task="task" @complete="completeTask" />
+  </div>
+</template>
+
+<script>
+import Task from '~/components/Task.vue';
+
+export default {
+  components: {
+    Task,
+  },
+  props: {
+    tasks: Array,
+    onComplete: Function,
+  },
+  methods: {
+    completeTask(taskId) {
+      this.onComplete(taskId);
+    },
+  },
+};
+</script>
